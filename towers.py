19a03725da5e74f9123c290ac942e79eb9dc3f25
Fr3zz0-ux <mateuszbogacki0@gmail.com>
2026-02-23 00:31:53 +0100

    dodano -atak -cooldown

--- towers.py
+++ towers.py
@@ -3,10 +3,13 @@ import math
 
 class Tower:
 
-    def __init__(self, radius, color, range, x_pos, y_pos):
+    def __init__(self, radius, color, towerRange, damage, cooldown, x_pos, y_pos):
         self.radius = radius
         self.color = color
-        self.range = range + radius
+        self.range = towerRange + radius
+        self.damage = damage
+        self.last_attack_time = 0
+        self.cooldown = cooldown
         self.x_pos = x_pos
         self.y_pos = y_pos
 
@@ -15,6 +18,15 @@ class Tower:
         pygame.draw.circle(screen, "gray", (self.x_pos, self.y_pos), radius=self.radius + self.range)
         pygame.draw.circle(screen, self.color, (self.x_pos, self.y_pos), radius=self.radius)
 
-    def atack(self, screen, enemy_pos, enemy_x, enemy_y):
-        if enemy_pos <= self.range:
-            pygame.draw.line(screen, "yellow", (self.x_pos, self.y_pos), (enemy_x, enemy_y))
+    def attack(self, screen, enemy, distance):
+
+        current_time = pygame.time.get_ticks()
+
+        if distance <= self.range:
+            pygame.draw.line(screen, "yellow", (self.x_pos, self.y_pos), (enemy.x, enemy.y))
+
+            if current_time - self.last_attack_time >= self.cooldown:
+                enemy.take_damage(self.damage)
+                print(enemy.health)
+
+                self.last_attack_time = current_time
